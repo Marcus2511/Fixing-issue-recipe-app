1d3fdcf274fc88f41889f996aa2678ec2fc7e706
github_Marcus2511 <ratofhamelin1125@gmail.com>
2023-01-09 22:05:36 +0700

    setup gh-pages

---
+++ src/Components/Meal.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import classes from "./Meal.module.css"
+import MealItem from "./MealItem";
+import RecipeIndex from "./RecipeIndex";
+const Meal=()=>{
+    const [url, setUrl]=useState("https:/www.themealdb.com/api/json/v1/1/search.php?f=d");
+    const [item, setItem]=useState();
+    const [show, setShow]=useState(false)
+    const [search, setSearch]=useState("")
+
+    const getrepcipe = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setItem(data.meals);
+        console.log(data.meals);
+        setShow(true);
+      };
+
+    useEffect(()=>{
+        getrepcipe();
+    },[url]);
+    console.log(show);
+
+    const setIndex=(alpha)=>{
+        setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`)
+    }
+
+    const indexSearch=(KB)=>{
+        if(KB.key==="Enter")
+        setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+    }
+    return(
+        <div className={classes.main}>
+            <div className={classes.heading}>
+                <h1>Search your food Recipe</h1>
+                <h3>"Patience is the secret to good food."</h3>
+                <h4>Gail Simmons</h4>
+            </div>
+            <div className={classes.searchbox}>
+                <input type="search" className={classes["search-bar"]} onChange={e=>setSearch(e.target.value)} onKeyPress={indexSearch}></input>
+            </div>
+            <div className={classes.container}>
+                {
+                    show? <MealItem data={item}/>:"NOT FOUND"
+                }
+            </div>
+            <div className={classes.indexcontainer}>
+                <RecipeIndex alphaIndex={(alpha)=>setIndex(alpha)}/>
+            </div>
+        </div>
+    );
+};
+export default Meal;
